e388c5f0b0383750f9a7a25c71ae030a74b92611
陈晓辉 <529912921@qq.com>
2018-09-17 15:33:33 +0800

    转场?

---
+++ CXNavigation/Transition(转场)/CXAnimatedTransitionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  CXAnimatedTransitionsViewController.h
+//  Navigation(导航)
+//
+//  Created by 陈晓辉 on 2018/8/30.
+//  Copyright © 2018年 陈晓辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CXAnimatedTransitionsViewController : UIViewController
+
+@end
---
+++ CXNavigation/Transition(转场)/CXAnimatedTransitionsViewController.m
@@ -0,0 +1,39 @@
+//
+//  CXAnimatedTransitionsViewController.m
+//  Navigation(导航)
+//
+//  Created by 陈晓辉 on 2018/8/30.
+//  Copyright © 2018年 陈晓辉. All rights reserved.
+//
+
+#import "CXAnimatedTransitionsViewController.h"
+
+@interface CXAnimatedTransitionsViewController ()
+
+@end
+
+@implementation CXAnimatedTransitionsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.navigationItem.title = @"AnimatedTransitions";
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
